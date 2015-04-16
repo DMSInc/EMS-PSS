96955f0bc2e3de937715e47ce44314a72297f82c
xiaodon1 <Xmeng-cc@2A213-C06.conestogac.on.ca>
2015-04-16 11:44:21 -0400

    Change database

--- loginUsers.sql
+++ loginUsers.sql
@@ -24,3 +24,41 @@ Salary varchar(40),
 primary Key(SIN)
 
 );
+
+Create table IF NOT EXISTS PERSON(
+ID integer NOT null auto_increment,
+FIRSTNAME VARCHAR(40),
+LASTNAME VARCHAR(40),
+SIN		VARCHAR(10),
+DATEOFBIRTH VARCHAR(40),
+PRIMARY KEY(ID)
+);
+
+
+Create table if not exists company(
+ID integer not null auto_increment,
+CORPORATIONNAME VARCHAR(40),
+PRIMARY KEY(ID)
+);
+
+Create table if not exists  employee(
+ID integer not null auto_increment,
+company_id integer not null,
+person_id  integer not null,
+Employeestatus varchar(40),
+ReasonForLeave varchar(40),
+EmployType     varchar(10), 
+Foreign key fk_companyid(company_id) references company(ID),
+Foreign key fk_personid(person_id) references person(ID),
+primary key(ID)
+);
+
+Create table if not exists FulltimeEmployee(
+ID integer not null auto_increment,
+employee_id integer not null,
+dateofhire	varchar(40),
+dateoftermination varchar(40),
+salary varchar(40),
+Foreign key fk_employeeid(employee_id) references employee(id),
+primary key(ID)
+);
